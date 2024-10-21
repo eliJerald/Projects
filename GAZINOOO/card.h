1f01c20965316e8c0a0274543577dc451682f7ea
Eli Rosales <ejerald.cr@gmail.com>
2024-10-21 13:10:43 -0700

    add card.h

---
+++ GAZINOOO/card.h
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Card{
+    int val;
+    string face;
+    string suite;   
+    bool color;         //1 = red       0 = black
+    int status;         //0 = in deck   1 = in play     2 = discard pile
+    
+}Deck [53];
---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
+int main(){
+    //&13 + 1 is 1-13
+    //%13 is 0-12;
+    bool flag = false;
+    int inc= 0;
+    while(!flag){
+        srand(time(0));
+        int num = rand() % 13+1;
+        cout<<inc<<endl;
+        cout<<num;
+        inc++;
+
+        if (num == 13){
+            flag = true;
+        }
+    }
+
+
+    
+    return 0;
+}
